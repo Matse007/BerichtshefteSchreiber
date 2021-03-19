864da439f04730f1902da3f49bbdb87a902ccde3
Emily <66444710+Matse007@users.noreply.github.com>
2021-03-19 10:18:16 +0100

    Revert "Devbuild"

--- Berichtsheft/Program.cs
+++ Berichtsheft/Program.cs
@@ -3,25 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Serilog;
 
 namespace Berichtsheft
 {
     static class Program
     {
         /// <summary>
-        /// The main start for the entire program. We are deleting the logfile for a previous session, if one exists.
-        /// Afterwards a new logger and logfile is created and we are loading the Form.
+        /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            System.IO.File.Delete("logs/BerichtshefteSchreiberLog.txt");
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.File("logs/BerichtshefteSchreiberLog.txt", shared: true, retainedFileCountLimit: 1)
-                .CreateLogger();
-            Log.Information("Logger initialized");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
